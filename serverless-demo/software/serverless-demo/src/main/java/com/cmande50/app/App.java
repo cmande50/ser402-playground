fc0cf309bd26d83e7968b3652781ab4d1fae62dd
Chris Anderson <cmande50@asu.edu>
2024-10-03 15:33:04 -0400

    Clean up Lambda code

--- serverless-demo/software/serverless-demo/src/main/java/com/cmande50/app/App.java
+++ serverless-demo/software/serverless-demo/src/main/java/com/cmande50/app/App.java
@@ -17,8 +17,10 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
  */
 public class App implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
     private final DynamoDbAsyncClient dynamoDbClient;
+    private LambdaLogger logger;
 
     public App() {
+        
         // Initialize the SDK client outside of the handler method so that it can be reused for subsequent invocations.
         // It is initialized when the class is loaded.
         dynamoDbClient = DependencyFactory.dynamoDbClient();
@@ -27,16 +29,23 @@ public class App implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HT
 
     @Override
     public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
-    LambdaLogger logger = context.getLogger();
+    this.logger = context.getLogger();
     logger.log("EVENT TYPE: " + event.getClass().toString());
+
+    return handleHello();
+  }
+
+  private APIGatewayV2HTTPResponse handleHello() {
     APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
-    response.setIsBase64Encoded(false);
-    response.setStatusCode(200);
+
     HashMap<String, String> headers = new HashMap<String, String>();
     headers.put("Content-Type", "text/html");
+
     response.setHeaders(headers);
-    String body = event.getBody() != null ? event.getBody() : "Empty body";
+    response.setIsBase64Encoded(false);
+    response.setStatusCode(200);
     response.setBody("Hello World");
+
     return response;
   }
 }
