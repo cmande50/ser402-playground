45f22a296f1c221ae4372f7e87509d74080d4582
Christopher Anderson <cmande50@asu.edu>
2024-10-19 15:57:20 -0400

    Advanced React Mod 1 - basic-list (fix)

--- AdvancedReact/basic-list/src/DessertsList.js
+++ AdvancedReact/basic-list/src/DessertsList.js
@@ -1,16 +1,23 @@
 function DessertsList(props) {
   // Implement the component here.
 
-  const lowCalDesserts = props.data.map((d) => {
-    if (d.calories <= 500) {
-      return <li>${d.name} - ${d.calories} cal.</li>
-    }
-  });
+  const lowCalDesserts = props.data 
+    .filter((dessert) => { 
+      return dessert.calories < 500; 
+    }) 
+    .sort((a, b) => { 
+      return a.calories - b.calories; 
+    }) 
+    .map((dessert) => { 
+      return ( 
+        <li> 
+          {dessert.name} - {dessert.calories} cal 
+        </li> 
+      ); 
+    });
 
   return (
-    <ul>
-      {lowCalDesserts}
-    </ul>
+    <ul>{lowCalDesserts}</ul>
   );
 }
 
